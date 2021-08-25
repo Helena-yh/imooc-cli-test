08d16f01b8d17db380632d53b30b3cd38f63a380
yanhan <yanhan@rongcloud.cn>
2021-08-25 17:24:42 +0800

    imooc-cli first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="imooc-cli-test">vue3 template</div>
+  <div>text</div>
+</template>
+
+<script>
+export default {
+  name: 'ImoocCliTest',
+}
+</script>
+
+<style>
+#imooc-cli-test {
+  width: 100%;
+  height: 100%;
+}
+</style>
